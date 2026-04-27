b32bfc0364bedd800dbe3836a45230c8254ca4c4
Sarthak Sachan <SSachan@flexera.com>
2026-04-27 16:32:10 +0530

    Fix-Project Hierarchy graph

--- project_heirarchy.py
+++ project_heirarchy.py
@@ -21,44 +21,64 @@ def create_project_heirarchy(baseURL, authToken, projectID, includeChildProjects
     projectHierarchy = common.api.project.get_child_projects.get_child_projects_recursively(baseURL, projectID, authToken)
     projectName = projectHierarchy["name"]
 
+    logger.info("=" * 80)
+    logger.info("Building hierarchy for root project: %s (ID: %s)" % (projectName, projectID))
+    logger.info("=" * 80)
+
     # Create a list of project data sorted by the project name at each level for report display  
     # Add details for the parent node
     nodeDetails = {}
-    nodeDetails["projectID"] = projectID
+    nodeDetails["projectID"] = str(projectID)
+    nodeDetails["uniqueID"] = str(projectID)  # Root node unique ID is just the project ID
     nodeDetails["parent"] = "#"  # The root node
     nodeDetails["projectName"] = projectName
-    nodeDetails["projectID"] = projectID
     nodeDetails["projectLink"] = baseURL + "/codeinsight/FNCI#myprojectdetails/?id=" + str(projectID) + "&tab=projectInventory"
     nodeDetails["inventoryLinkBase"] = nodeDetails["projectLink"]  + "&pinv="
 
+    logger.info("Adding ROOT: name='%s', id='%s', uniqueID='%s', parent='%s'" % (nodeDetails["projectName"], nodeDetails["projectID"], nodeDetails["uniqueID"], nodeDetails["parent"]))
     projectList.append(nodeDetails)
 
     if includeChildProjects:
-        projectList = manage_child_projects(projectHierarchy, projectID, projectList, baseURL)
+        projectList = manage_child_projects(projectHierarchy, str(projectID), str(projectID), projectList, baseURL)
 
+    logger.info("=" * 80)
+    logger.info("FINAL PROJECT HIERARCHY LIST (%d projects):" % len(projectList))
+    logger.info("=" * 80)
+    for idx, proj in enumerate(projectList):
+        logger.info("[%2d] name='%s', id='%s', uniqueID='%s', parent='%s'" % (idx, proj["projectName"], proj["projectID"], proj.get("uniqueID", proj["projectID"]), proj["parent"]))
+    logger.info("=" * 80)
 
     return projectList
 
 
 #----------------------------------------------#
-def manage_child_projects(project, parentID, projectList, baseURL):
-    logger.debug("Entering manage_child_projects")
+def manage_child_projects(project, parentID, parentUniqueID, projectList, baseURL):
+    logger.debug("Entering manage_child_projects for parentID: %s, parentUniqueID: %s" % (parentID, parentUniqueID))
 
     # Are there more child projects for this project?
     if len(project["childProject"]):
+        logger.info("  Parent '%s' (ID=%s, UniqueID=%s) has %d child projects" % (project.get("name", "?"), parentID, parentUniqueID, len(project["childProject"])))
 
         # Sort by project name of child projects
         for childProject in sorted(project["childProject"], key = lambda i: i['name'] ) :
 
+            childID = str(childProject["id"])
+            # Create unique ID by combining parent's unique ID with child's ID
+            # This ensures each node in the tree has a unique identifier
+            childUniqueID = parentUniqueID + "_" + childID
+
             nodeDetails = {}
-            nodeDetails["projectID"] = str(childProject["id"])
-            nodeDetails["parent"] = parentID
+            nodeDetails["projectID"] = childID
+            nodeDetails["uniqueID"] = childUniqueID
+            nodeDetails["parent"] = parentUniqueID  # Use parent's unique ID
             nodeDetails["projectName"] = childProject["name"]
-            nodeDetails["projectLink"] = baseURL + "/codeinsight/FNCI#myprojectdetails/?id=" + str(childProject["id"]) + "&tab=projectInventory"
+            nodeDetails["projectLink"] = baseURL + "/codeinsight/FNCI#myprojectdetails/?id=" + childID + "&tab=projectInventory"
             nodeDetails["inventoryLinkBase"] = nodeDetails["projectLink"]  + "&pinv="
 
+            logger.info("    Adding CHILD: name='%s', id='%s', uniqueID='%s', parent='%s'" % (nodeDetails["projectName"], nodeDetails["projectID"], nodeDetails["uniqueID"], nodeDetails["parent"]))
             projectList.append( nodeDetails )
 
-            manage_child_projects(childProject, childProject["id"], projectList, baseURL)
+            # Recurse with child's unique ID
+            manage_child_projects(childProject, childID, childUniqueID, projectList, baseURL)
 
     return projectList
